423673d011147afe5db0944bf93add9f4352284d
Simon Arvidsson <simon.arvidsson@gmail.com>
2023-03-19 21:55:18 +0100

    adding react three fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import Layout from "./components/Layout";
+import Box from "./components/Box";
+import { Canvas } from '@react-three/fiber';
+import React from 'react';
+
 function App() {
   return (
     <Layout>
       <div className="h-screen bg-stone-400 items-center justify-center flex">
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
         <span className="text-4xl">Placeholder</span>
       </div>
     </Layout>
