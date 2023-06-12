397e729ea28b279a91f7a849bcccd0076b8fa69d
Joseph Chamochumbi <sephxd1234@gmail.com>
2023-06-12 09:08:56 +0200

    chore: Setup Prettier to have similar coding style across the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import MainContent from "./components/MainContent";
-import { ThemeProvider, useTheme } from '@emotion/react'
+import { ThemeProvider, useTheme } from "@emotion/react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
@@ -7,27 +7,26 @@ import theme from "./emotion.d";
 import HeadingWithSubheading from "./components/HeadingWithSubheading";
 
 function App() {
-
-  return (
-    <ThemeProvider theme={theme}>
-      <Navbar />
-      <Hero />
-      <HeadingWithSubheading
-        backgroundColor={theme.backgroundColors.charcoal}
-        textColor={theme.backgroundColors.livingRoom}
-        text="We are Sprout"
-        subheading="Welcome to our universe. A growing ecosystem of collaborating people, ideas and initiatives"
-      />
-      <MainContent />
-      <HeadingWithSubheading 
-        backgroundColor={theme.backgroundColors.livingRoom} 
-        textColor={theme.backgroundColors.charcoal} 
-        text="Next up" 
-        subheading="What would you like to know" 
-      />
-      <Footer />
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Navbar />
+            <Hero />
+            <HeadingWithSubheading
+                backgroundColor={theme.backgroundColors.charcoal}
+                textColor={theme.backgroundColors.livingRoom}
+                text="We are Sprout"
+                subheading="Welcome to our universe. A growing ecosystem of collaborating people, ideas and initiatives"
+            />
+            <MainContent />
+            <HeadingWithSubheading
+                backgroundColor={theme.backgroundColors.livingRoom}
+                textColor={theme.backgroundColors.charcoal}
+                text="Next up"
+                subheading="What would you like to know"
+            />
+            <Footer />
+        </ThemeProvider>
+    );
 }
 
 export default App;
