749f0ca13dc545aade63023a8a16d59c43452575
Joseph Chamochumbi <sephxd1234@gmail.com>
2023-06-19 22:53:19 +0200

    chore: Include emotion swc plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxImportSource: '@emotion/react' })],
-})
+    plugins: [
+        react({
+            jsxImportSource: "@emotion/react",
+            plugins: [
+                [
+                    "@swc/plugin-emotion",
+                    {
+                        // default is true. It will be disabled when build type is production.
+                        sourceMap: false,
+                        // default is 'dev-only'.
+                        // autoLabel: "never" | "dev-only" | "always",
+                        autoLabel: "dev-only",
+
+                        // default is '[local]'.
+                        // Allowed values: `[local]` `[filename]` and `[dirname]`
+                        // This option only works when autoLabel is set to 'dev-only' or 'always'.
+                        // It allows you to define the format of the resulting label.
+                        // The format is defined via string where variable parts are enclosed in square brackets [].
+                        // For example labelFormat: "my-classname--[local]", where [local] will be replaced with the name of the variable the result is assigned to.
+                        // labelFormat: string,
+                    },
+                ],
+            ],
+        }),
+    ],
+});
