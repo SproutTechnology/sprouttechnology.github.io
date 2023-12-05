c9d134bd15ecb691aa435e73889fd5816ed24f95
Tobias <zizzozz@hotmail.com>
2023-12-05 13:05:02 +0100

    Wave-placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import { ThemeProvider } from "@emotion/react";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import WeAreSprout from "./components/WeAreSprout"
-
+import Wave from "./components/Wave";
 function App() {
     return (
         <ThemeProvider theme={theme}>
             <Navbar></Navbar>
+            <Wave></Wave>
             <Hero></Hero>
             <WeAreSprout></WeAreSprout>
         </ThemeProvider>
---
+++ src/components/Wave.tsx
@@ -0,0 +1,13 @@
+import SectionLayout from "./SectionLayout";
+
+function Wave() {
+    return (
+        <SectionLayout>
+         <div>REPLACE ME WITH WAVE</div>
+        </SectionLayout>
+    );
+}
+
+
+export default Wave;
+
