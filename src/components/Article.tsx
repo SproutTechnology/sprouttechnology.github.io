b765bb2df98b20afdbe40db12137027e1397f029
Magnus Unger <magnus_unger@hotmail.com>
2024-01-25 11:03:53 +0100

    Added editorconfig and updated prettier config

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,5 +1,3 @@
-
-
 import styled from "@emotion/styled";
 import { ReactNode } from "react";
 import { mq } from "../theme";
@@ -12,33 +10,30 @@ interface Props {
 
 function Article({ heading, children, headingRight = false }: Props) {
     return (
-
         <StyledArticle headingRight={headingRight}>
             {headingRight ? <StyledHeadingRight>{heading}</StyledHeadingRight> : <StyledHeadingLeft>{heading}</StyledHeadingLeft>}
             <article>{children}</article>
         </StyledArticle>
-
     );
 }
-
 
 export default Article;
 
 const StyledHeadingLeft = styled.h2`
-    font-weight : 700;
-`
+    font-weight: 700;
+`;
 const StyledHeadingRight = styled.h2`
-    font-weight : 700;
-    text-align : right;
-`
+    font-weight: 700;
+    text-align: right;
+`;
 
-const StyledArticle = styled.div<{headingRight: boolean}>`
-${({ theme, headingRight }) => `
+const StyledArticle = styled.div<{ headingRight: boolean }>`
+    ${({ theme, headingRight }) => `
     min-width : 250px;
     flex-basis : 40%;
     flex-grow : 2;
     display : flex;
-    flex-direction : ${headingRight ? "column-reverse": "column"};
+    flex-direction : ${headingRight ? "column-reverse" : "column"};
     gap : 2rem;
 
     ${mq["sm"]} { 
@@ -58,4 +53,4 @@ ${({ theme, headingRight }) => `
         }
     }
     `}
-`
+`;
--- src/hooks/useCurrentView.tsx
+++ src/hooks/useCurrentView.tsx
@@ -1,40 +1,40 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 export function useCurrentView() {
-    const offsetTop = 100
+    const offsetTop = 100;
 
-    const [currentView, setCurrentView] = useState<string>('')
+    const [currentView, setCurrentView] = useState<string>("");
 
     const handleScroll = () => {
-        let views = document.querySelectorAll('div[data-type="ViewContainer"]')
+        let views = document.querySelectorAll('div[data-type="ViewContainer"]');
 
         for (let index = 0; index < views.length; index++) {
-            const view = views[index]
+            const view = views[index];
 
-            var viewRect = view.getBoundingClientRect()
-            var top = viewRect.top + viewRect.height - offsetTop
+            var viewRect = view.getBoundingClientRect();
+            var top = viewRect.top + viewRect.height - offsetTop;
 
             if (top > 0 && top <= viewRect.height) {
-                setCurrentView(view.id || '')
+                setCurrentView(view.id || "");
             }
         }
-    }
+    };
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll, { passive: true })
+        window.addEventListener("scroll", handleScroll, { passive: true });
         return () => {
-            window.removeEventListener('scroll', handleScroll)
-        }
-    }, [])
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     useEffect(() => {
-        const location = window.location.toString().split('#')[0]
-        if (currentView === '') {
-            history.replaceState(null, '', location)
+        const location = window.location.toString().split("#")[0];
+        if (currentView === "") {
+            history.replaceState(null, "", location);
         } else {
-            history.replaceState(null, '', location + '#' + currentView)
+            history.replaceState(null, "", location + "#" + currentView);
         }
-    }, [currentView])
+    }, [currentView]);
 
-    return currentView
+    return currentView;
 }
