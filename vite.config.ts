d19eabc047da4302caf93a2832bb7df1c9df42d5
Magnus Unger <magnus_unger@hotmail.com>
2024-01-25 11:29:03 +0100

    Added tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,7 @@ export default defineConfig({
         open: true,
     },
     plugins: [
+        tsconfigPaths(),
         react({
             jsxImportSource: "@emotion/react",
             plugins: [
