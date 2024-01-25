05e03b3433a2a9c380cad3349265ad18d0e4003d
Magnus Unger <magnus_unger@hotmail.com>
2024-01-25 20:54:22 +0100

    Minor lint fixes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,12 @@ module.exports = {
     env: { browser: true, es2020: true },
     extends: [
         "eslint:recommended",
-        "plugin:prettier/recommended",
         "plugin:react/recommended",
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:react-hooks/recommended",
         "plugin:react/jsx-runtime",
+        "plugin:prettier/recommended",
     ],
     settings: {
         react: {
